672e187cb3f5dc46efe0850d7f0ea93f1ac68632
ARYANAMAN191179 <sonirehal110@gmail.com>
2024-07-04 21:13:45 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from highrise.__main__ import BotDefinition, main, import_module, arun
 import time
 
-room_id = "Room id in here"
-bot_token = "Bot token in here"
+room_id = "65e93ab505782ba2fe3745c7"
+bot_token = "559e0fc1599e679b49704bd55939f00dc57057a41a976826d52afebcee9b22ad"
 bot_file = "main"
 bot_class = "MyBot"
 
